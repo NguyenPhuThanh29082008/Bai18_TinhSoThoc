4f5e8f90e3d17753a9e94050019ed95e236e08b6
NguyenPhuThanh20082008 <nguyenphuthanhpch298@gmail.com>
2024-11-25 08:50:04 +0700

    Update

--- Bai18_TinhSoThoc.cpp
+++ Bai18_TinhSoThoc.cpp
@@ -6,20 +6,21 @@ using namespace std;
 
 int main()
 {
-    double o, tongthoc = 0, tong = 0;
+    int o;
+    double tongthoc = 0;
+
     cout << "Tinh So Thoc\n";
     cout << "Nhap so o: "; cin >> o; cout << endl;
 
 
-    for (int i = 1; i <= o; i++)
+    for (int i = 0; i <= o; i++)
     {
-        tongthoc = pow(2, i-1);
-        tong += tongthoc;
-        cout << "o thu " << i << " : " << tongthoc << endl;
+        tongthoc = tongthoc + pow(2, i);
+        cout << "o thu " << i + 1 << " co so thoc la: " << pow(2,i) << " hat thoc" << endl;
 
     }
 
-    cout << "Tong cac so thoc: " << tong << endl;
+    cout << "Tong cac so thoc: " << tongthoc << endl;
 
     return 0;
 }
